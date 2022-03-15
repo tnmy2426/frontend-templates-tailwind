7279351b1cdc53aeeff69cecc01ddcad89e3dee9
tnmy2426 <al.nahdi.tnmy@gmail.com>
2022-03-15 07:43:17 +0600

    Demo Design Complete. Ready to host.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,24 @@
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+   content: ["./public/**/*.html"],
+   theme: {
+      extend: {
+         colors: {
+            primary_clr: "#F7D031",
+            secondary_clr: "#f95157",
+            title_clr: "#1b2f4d",
+         },
+      },
+      container: {
+         center: true,
+         padding: {
+            DEFAULT: "1rem",
+            sm: "2rem",
+            md: "3rem",
+            lg: "4rem",
+            xl: "5rem",
+            "2xl": "6rem",
+         },
+      },
+   },
+   plugins: [],
+};
