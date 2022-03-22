f49c5fab2e7677f88e29d365c5cdbbf673f756ca
tnmy2426 <al.nahdi.tnmy@gmail.com>
2022-03-22 20:01:34 +0600

    Mobile nav btn & content added & designed. Navbar full funtionality added completely and working fine.

--- public/main.js
+++ public/main.js
@@ -5,6 +5,7 @@ window.onscroll = function () {
 
 var navbar = document.querySelector("nav");
 var content = document.querySelector("main");
+var overlay = document.getElementById("mobileNav");
 //  var footer = document.querySelector("footer");
 
 // Get the offset position of the navbar
@@ -15,6 +16,9 @@ function myFunction() {
    if (window.pageYOffset >= sticky_nav) {
       navbar.classList.add("sticky_nav");
       content.classList.add("main_content");
+
+      overlay.classList.remove("overlay");
+      overlay.classList.add("overlay_scroll");
       //    footer.classList.add("footer_c");
    } else {
       navbar.classList.remove("sticky_nav");
@@ -22,3 +26,35 @@ function myFunction() {
       //    footer.classList.remove("footer_c");
    }
 }
+
+// ACTIVE LINK
+
+// Get the container element
+var nav = document.querySelector("nav");
+
+// Get all buttons with class="btn" inside the container
+var nav_links = nav.getElementsByClassName("nav_link");
+
+// Loop through the buttons and add the active class to the current/clicked button
+for (var i = 0; i < nav_links.length; i++) {
+   nav_links[i].addEventListener("click", function () {
+      var current = document.getElementsByClassName("active_link");
+      current[0].className = current[0].className.replace(" active_link", "");
+      this.className += " active_link";
+   });
+}
+
+// Mobile Menu Function
+function openNav() {
+   document.getElementById("mobileNav").style.width = "80%";
+   document.getElementById("menu_btn").style.display = "none";
+   document.getElementById("cross_btn").style.display = "flex";
+}
+
+function closeNav() {
+   document.getElementById("mobileNav").style.width = "0%";
+   document.getElementById("menu_btn").style.display = "flex";
+   document.getElementById("cross_btn").style.display = "none";
+}
+
+// var menu_btn = document.getElementById("menu_btn")
